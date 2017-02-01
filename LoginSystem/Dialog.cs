d02f73a021dedf7fc0804fd19c8d25eaa831b954
Kajan <lkajan@hotmail.com>
2017-02-01 11:50:40 +0000

    Commit

---
+++ LoginSystem/Dialog.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LoginSystem
+{
+    class Dialogclass : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+            return view;
+
+        }
+    }
+}
--- LoginSystem/MainActivity.cs
+++ LoginSystem/MainActivity.cs
@@ -1,31 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 using Android.App;
 using Android.Content;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
+
+
 
 namespace LoginSystem
 {
     [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
+        private Button mButtnSignup;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
+            // Set our view from the "main" layout resource  
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            mButtnSignup = FindViewById<Button>(Resource.Id.sinup);
+            mButtnSignup.Click += (object sender, EventArgs args) =>
+            {
+                FragmentTransaction transcation = FragmentManager.BeginTransaction();
+                Dialogclass signup = new Dialogclass();
+                signup.Show(transcation, "Dialog Fragment");
+            };
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
         }
+
+
     }
 }
-
